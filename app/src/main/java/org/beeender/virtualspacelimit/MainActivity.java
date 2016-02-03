b8c53949f6b7524c6b8605b4d1a33ec683fba283
Chen Mulong <chenmulong@gmail.com>
2016-02-03 16:57:21 +0800

    mmap same amount of memory multiple times.

--- app/src/main/java/org/beeender/virtualspacelimit/MainActivity.java
+++ app/src/main/java/org/beeender/virtualspacelimit/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -17,9 +18,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private AtomicBoolean finished = new AtomicBoolean(false);
-    private AtomicLong mmapSize = new AtomicLong(0);
-    private long addr = -1;
+    private TextView threadCountsTextView;
+    private Button startButton;
+    private AtomicBoolean finished;
+    private AtomicLong mmapSize;
+    private long[] addrs;
+    private long[] sizes;
     Handler handler;
 
     @Override
@@ -39,39 +43,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
         handler = new Handler(Looper.myLooper());
+        startButton = (Button) findViewById(R.id.button);
+        threadCountsTextView = (TextView) findViewById(R.id.threadsCount);
         textView = (TextView) findViewById(R.id.textView);
-        textView.setText("Running...");
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!finished.get()) {
-                    doMmap();
-                    try {
-                        Thread.sleep(16L);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        thread.setPriority(Thread.MIN_PRIORITY);
-        thread.start();
     }
 
     private void doMmap() {
         long size = mmapSize.get() + 100 * 1024 * 1024;
-        long ret;
-        if (addr == -1) {
-            ret = MMap.mmap(size);
-        } else {
-            ret = MMap.mremap(addr, mmapSize.get(), size);
+        long ret = -1;
+        for (int i = 0; i < addrs.length; i++) {
+            long addr = addrs[i];
+            if (addr == -1) {
+                ret = MMap.mmap(size);
+            } else {
+                ret = MMap.mremap(addr, mmapSize.get(), size);
+            }
+            if (ret != -1) {
+                addrs[i] = ret;
+                sizes[i] = size;
+            } else {
+                break;
+            }
         }
         if (ret != -1) {
             mmapSize.set(size);
         } else {
             finished.set(true);
-            MMap.munmap(addr, mmapSize.get());
-            addr = -1;
         }
         handler.post(new Runnable() {
             @Override
@@ -107,7 +104,45 @@ public class MainActivity extends AppCompatActivity {
         String text = "Current mmap size is " + mmapSize.get() / 1024 / 1024 + "MB";
         if (finished.get()) {
             text += " - DONE";
+            startButton.setEnabled(true);
         }
         textView.setText(text);
+    }
+
+    public void onClick(View view) {
+        startButton.setEnabled(false);
+
+        if (addrs != null) {
+            for (int i = 0; i < addrs.length; i++) {
+                MMap.munmap(addrs[i], sizes[i]);
+            }
+        }
+
+        int counts = Integer.valueOf(threadCountsTextView.getText().toString());
+        addrs = new long[counts];
+        sizes = new long[counts];
+        for (int i = 0; i < addrs.length; i++) {
+            addrs[i] = -1;
+            sizes[i] = 0;
+        }
+
+        finished = new AtomicBoolean(false);
+        mmapSize = new AtomicLong(0);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!finished.get()) {
+                    doMmap();
+                    try {
+                        Thread.sleep(16L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
     }
 }
